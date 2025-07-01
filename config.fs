0b3de058e31fa1008fc618f645dafcaa8a60d020
Bhanu Vivek Matsa <bmasta@qti.qualcomm.com>
2025-07-01 17:30:11 +0530

    sm8850-common: sensors.debug service config change
    Change-Id: I5db3ca65ad77bab0379b0b637c19d814fafba237

--- config.fs
+++ config.fs
@@ -217,7 +217,7 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
-[vendor/bin/sensors.qti]
+[vendor/bin/sensors.debug]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
