104b5044f122ddc94d64760b95f5bdc1774d5813
dianlujitao <dianlujitao@lineageos.org>
2026-02-07 13:07:12 +0000

    sm8850-common: Switch to AIDL compatibility matrix
    Change-Id: I628ca70be28452c5f347ed21f9ce7632de07a421

--- common.mk
+++ common.mk
@@ -479,7 +479,7 @@ DEVICE_MANIFEST_FILE := \
     $(AUDIO_HAL_DIR)/configs/common/manifest_non_qmaa.xml \
     $(AUDIO_HAL_DIR)/configs/common/manifest_non_qmaa_extn.xml \
     $(LOCAL_PATH)/vintf/manifest_pineapple.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix_aidl.xml
 
 ifneq ($(TARGET_IS_TABLET),true)
 DEVICE_MANIFEST_FILE += \
