42d33fdcaf4cd910f8c6103957867bdd1611b85f
chandu078 <chandudyavanapelli03@gmail.com>
2026-02-07 05:12:08 +0000

    sm8850-common: Switch to prebuilt kernel
    Change-Id: I0678256fc70af5168af3e479842ce3c9bc3cd188

--- common.mk
+++ common.mk
@@ -230,8 +230,8 @@ $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_oplus)
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
-PRODUCT_COPY_FILES += \
-    kernel/oneplus/sm8850/modules.systemdlkm_blocklist.msm.pineapple:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/system_dlkm.modules.blocklist
+#PRODUCT_COPY_FILES += \
+#    kernel/oneplus/sm8850/modules.systemdlkm_blocklist.msm.pineapple:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/system_dlkm.modules.blocklist
 
 # Keymint
 PRODUCT_PACKAGES += \
